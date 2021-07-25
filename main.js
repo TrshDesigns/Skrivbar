ec0e1bf394ca2ae79e509176c0d579eeb0f870c1
TrshDesigns <trshdesigns@outlook.com>
2021-07-25 18:07:54 -0300

    changes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
+const { get } = require('http');
 const path = require('path');
+const { stringify } = require('querystring');
 
 
 function createWindow () {
@@ -60,12 +62,6 @@ function execCmd (command) {
 function execCommandWithArg (command, arg){
   richTextField.document.execCommand(command, false, arg);
 }
-/*on load show current folder*/
-function listcurrentfolder(){
- let currentfolder = "Home";
- let currentfolderdisplayer = document.getElementById("current-folder-displayer")
- currentfolderdisplayer.innerText = currentfolder;
-}
 //Hide or show list organizer
 function hidelistorganizer() {
   const listorganizer = document.getElementById("list");
@@ -77,27 +73,49 @@ function hidelistorganizer() {
     hidelistorganizer.style.margin = "2px 0px 0px 5px"
    } else {
     listorganizer.style.display = "block";
-        hidelistorganizer.style.margin = "2px 5px 0px 5px"
+        hidelistorganizer.style.margin = "2px 0px 0px 5px"
    }
   }
-//tool tips
-function showbox(){}
-function hidebox(){}
-//hub
-function hub(){
-  const header  = document.getElementById("header-bar");
-  const main = document.getElementById("main");
-  if (header.style.height = "30px"){
-    header.style.height = "80vh";
+
+//limit the amount of characters on the input name in the list 
+//creates a new list when the button addButton is clicked
+function createNewList(){
+  const listItem = document.createElement("li");
+  const listItems = document.getElementById("listitems");
+  listItem.className = "list-item" ;
+  listItem.innerHTML = `<input class="list-name" value="Re Write Me!" type="text" maxlength="30"><button class="deleteListItem" onclick="deleteListItem()"><svg width="30" height="30" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M24.4437 24.5566L38.5858 38.6988L41.4142 41.5272L55.5564 55.6693" stroke="#F2994A" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M55.5563 24.5566L41.4142 38.6988L38.5858 41.5272L24.4436 55.6693" stroke="#F2C94C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+</svg></button>`;
+  listItems.appendChild(listItem);
+}
+//delete a list item
+function deleteListItem(){
+  const listItems = document.getElementById("listitems");
+  const listItem = document.getElementsByClassName("list-item");
+  //delete the exact item in wicth the deleteListItem function was called
+  listItem[0].remove();
+}
+//extends the header and hides the main element
+function extend(){
+  const header = documentGetElementById("header");
+  const main = documentGetElementById("main");
+  if (main.style.display = "flex") {
     main.style.display = "none";
-  } if(header.style.height > "30px"){
-    header.style.height = "30px"
+    header.style.height = "100vh";
+  } else { 
+    main.style.display = "flex";
+    header.style.height = "30px";
   }
 }
 //hidefiledisplayer
 function hidefiledisplayer(){
   const filedisplayer  = document.getElementById("file-displayer")
-    filedisplayer.style.display === "none";
+  if (filedisplayer.style.display === "block") {
+    filedisplayer.style.display = "none";
+  } else {
+    filedisplayer.style.display = "block";
+  }
 }
 //Hide or Show Organizer
 function hideorganizer() {
@@ -116,52 +134,4 @@ if (organizer.style.display === "block") {
   organizer.style.display = "block";
  }
 }
-let el, i;
-let data = [
-  { firstname: "", lastname: "" }
-];
-let panel = document.querySelector("#panel");
 
-
-function clearForm() {
-  document.querySelector("#fname").value = "";
-  document.querySelector("#lname").value = "";
-}
-
-function renderItem() {
-
-  panel.textContent = "";
-  data.forEach(x => {
-    el = document.createElement("option");
-    el.innerText = `${x.firstname} ${x.lastname}`;
-    panel.append(el);
-  });
-}
-/*Functions to add or remove items on the organizer */
-function create() {
-  let fn = document.querySelector("#fname").value;
-  let ln = document.querySelector("#lname").value;
-  data = [...data, { firstname: fn, lastname: ln }];
-  clearForm();
-  console.log(data)
-  renderItem();
-}
-
-function panelClick() {
-  i = panel.selectedIndex;
-  document.querySelector("#fname").value = data[i].firstname;
-  document.querySelector("#lname").value = data[i].lastname;
-}
-
-function update() {
-  data[i].firstname = document.querySelector("#fname").value;
-  data[i].lastname = document.querySelector("#lname").value;
-  renderItem();
-}
-
-function deleteItem() {
-  data.splice(i, 1);
-  renderItem();
-}
-
- renderItem();
