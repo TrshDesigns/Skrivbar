91e96a61ac9db834b8b66d1bace5c3f564059271
TrshDesigns <trshdesigns@outlook.com>
2021-09-21 01:39:24 -0300

    adjusting cache directories

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,36 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-	({request}) => request.destination === 'image',
-	new workbox.strategies.NetworkFirst()
-	
+	/*create a register route and save all the css and html files along with js
+	in the cache*/
+	new RegExp('style.css'),
+	workbox.strategies.cacheFirst({
+		cacheName: 'css-cache',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 20,
+				maxAgeSeconds: 30 * 24 * 60 * 60,
+			}),
+		],
+	})
+	,new RegExp('index.html'),
+	workbox.strategies.cacheFirst({
+		cacheName: 'css-cache',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 20,
+				maxAgeSeconds: 30 * 24 * 60 * 60,
+			}),
+		],
+	})
+	, new RegExp("main.js"),
+	workbox.strategies.cacheFirst({
+		cacheName: 'js-cache',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 20,
+				maxAgeSeconds: 30 * 24 * 60 * 60,
+				}),
+			],
+		})
 );
